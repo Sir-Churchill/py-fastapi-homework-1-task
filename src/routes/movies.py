74e26aa549177d5272ac2f42f903b7304628a53e
Sir-Churchill <trickster@MacBook-Air-Trickster.local>
2025-11-01 19:40:52 +0100

    Solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,64 @@
+import math
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db, MovieModel
 
+from schemas import MovieListResponseSchema, MovieDetailResponseSchema, PaginatedMoviesResponse
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/", response_model=PaginatedMoviesResponse)
+async def get_movies(
+        page: int = Query(1, ge=1),
+        per_page: int = Query(10, ge=1),
+        db: AsyncSession = Depends(get_db),
+):
+    offset = (page - 1) * per_page
+
+    result = await db.execute(select(MovieModel).offset(offset).limit(per_page))
+    movies = result.scalars().all()
+
+    total_result = await db.execute(select(func.count()).select_from(MovieModel))
+    total_items = total_result.scalar()
+
+    total_pages = math.ceil(total_items / per_page)
+
+    error_message = {
+        "detail": [
+            {
+                "loc": ["query", "page"],
+                "msg": "ensure this value is greater than or equal to 1",
+                "type": "value_error.number.not_ge"
+            }
+        ]
+    }
+
+    prev_page = f"/api/v1/theater/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"/api/v1/theater/movies/?page={page + 1}&per_page={per_page}" \
+        if offset + len(movies) < total_items else None
+
+    response = {
+        "movies": movies,
+        "prev_page": prev_page,
+        "next_page": next_page,
+        "total_pages": total_pages,
+        "total_items": total_items,
+    }
+
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
+    elif per_page < 0 or page < 0:
+        return error_message
+    return response
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(MovieModel).where(MovieModel.id == movie_id))
+    movie = result.scalars().first()
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
+    return movie
